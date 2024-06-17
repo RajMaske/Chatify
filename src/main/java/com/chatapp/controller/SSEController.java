b6f33b4d5952a8fe039819d7941b4b28c9249db8
Namanand Maske <124178623+RajMaske@users.noreply.github.com>
2024-06-17 20:34:06 +0530

    file updated

---
+++ src/main/java/com/chatapp/controller/SSEController.java
@@ -0,0 +1,22 @@
+package com.chatapp.controller;
+
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+public class SSEController {
+
+    @GetMapping(value = "/sse", produces = "text/event-stream")
+    public Flux<ServerSentEvent<String>> streamEvents() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> ServerSentEvent.<String>builder()
+                        .id(String.valueOf(sequence))
+                        .event("message")
+                        .data("Hello SSE " + sequence)
+                        .build());
+    }
+}
