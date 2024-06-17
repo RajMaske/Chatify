3f18f4a21ae327858bbdfca0d5daa2ffa0ae208a
Namanand Maske <124178623+RajMaske@users.noreply.github.com>
2024-06-17 21:51:59 +0530

    file updated

--- src/main/java/com/chatapp/config/WebMvcConfig.java
+++ src/main/java/com/chatapp/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.chatapp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index"); // Maps the root URL to index.html
         registry.addViewController("/index").setViewName("index"); // Maps /index to index.html
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/chat-websocket")
+                .allowedOrigins("https://*.example.com")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(true);
     }
 }
