4990706d7aa5223ec615cd555a6f9092bc74d33d
Namanand Maske <124178623+RajMaske@users.noreply.github.com>
2024-06-17 21:07:19 +0530

    file updated

---
+++ src/main/java/com/chatapp/config/CustomMessageConverter.java
@@ -0,0 +1,7 @@
+package com.chatapp.config;
+
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+
+public class CustomMessageConverter extends MappingJackson2MessageConverter {
+    // Customize serialization/deserialization settings if needed
+}
--- src/main/java/com/chatapp/config/WebSocketConfig.java
+++ src/main/java/com/chatapp/config/WebSocketConfig.java
@@ -1,6 +1,10 @@
 package com.chatapp.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -20,4 +24,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> converters) {
+        // Register the custom converter
+        MappingJackson2MessageConverter converter = new CustomMessageConverter();
+        converters.add(converter);
+        
+        return true; // Set to 'true' if you don't want to add default message converters
+    }
+
 }
